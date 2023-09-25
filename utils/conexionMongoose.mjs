a613e41076e31ed9c18cf6c78dab0109cae522a6
Sebastian Benitez Mandirola <55502717+bmsebastian2@users.noreply.github.com>
2023-09-25 15:25:50 -0300

    connect

--- utils/conexionMongoose.mjs
+++ utils/conexionMongoose.mjs
@@ -6,8 +6,8 @@ const uri =
   "mongodb://bmsebastian2:Ab15415958@ac-79zezp4-shard-00-00.fhwg3s0.mongodb.net:27017,ac-79zezp4-shard-00-01.fhwg3s0.mongodb.net:27017,ac-79zezp4-shard-00-02.fhwg3s0.mongodb.net:27017/?ssl=true&replicaSet=atlas-loit2h-shard-0&authSource=admin&retryWrites=true&w=majority";
 export const makeConexion = () => {
   mongoose
-    .connect(`mongodb://${server}/${database}`)
-    //.connect(uri)
+    //.connect(`mongodb://${server}/${database}`)
+    .connect(uri)
     .then(() => {
       console.log("Database connection successful");
     })
